dbb41428396ff2a5f14ba1393b38abb5d8b02d15
luwol03 <luwol03@gmail.com>
2020-11-23 16:00:16 +0100

    🎉 released v1.0.0

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ from subprocess import getoutput  # skipcq: BAN-B404
 
 import requests
 
-VERSION = getoutput("cat VERSION 2>/dev/null || git describe").lstrip("v")
+VERSION = getoutput("cat VERSION 2>/dev/null || git describe --tags").lstrip("v")
 CONTRIBUTORS = []
 GITHUB_LINK = "https://github.com/luwol03/applybot"
 AVATAR_URL = "https://github.com/luwol03.png"
